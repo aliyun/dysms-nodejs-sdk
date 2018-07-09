23da6bcb3ec9f315d837dcab525aab4523de28d7
ql434 <kunqian434@gmail.com>
2018-07-09 19:54:29 +0800

    feat: 直接引用编译后的版本

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 'use strict';
-function supportAsyncFunctions() {
-  try {
-    new Function('(async function () {})()');
-    return true;
-  } catch (ex) {
-    return false;
-  }
-}
-module.exports = supportAsyncFunctions() ?
-  require('./src/client.js') : require('./lib/client.js');
+module.exports = require('./lib/client.js');
